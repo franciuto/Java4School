8ca29f1b93647057104a02a4d89b33154e7f352f
Franciuto <francesco.traversetolo@gmail.com>
2026-02-02 01:02:11 +0100

    mvn template for scenebuilder

---
+++ maven-javafx-scenebuilder/src/main/java/Main.java
@@ -0,0 +1,24 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("JavaFX Scene Builder App");
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("MainWindow.fxml"));
+        BorderPane root = loader.load();
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
